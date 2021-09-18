b4c1aebec27b28ca4d88399937f0de404ce71f8a
Mikhail-Bunkov <bunko04@bk.ru>
2021-09-19 01:39:52 +1000

    lessonFix

--- src/main/java/ru/bunkov/lesson3/queue/Deque.java
+++
@@ -1,9 +0,0 @@
-package ru.bunkov.lesson3.queue;
-
-public interface Deque<E> extends Queue<E>{
-    boolean insertRight(E value);
-    boolean insertLeft(E value);
-    E peakTail();
-    E removeRight();
-    E removeLeft();
-    }
---
+++ src/main/java/ru/bunkov/lesson3/stack/Stack.java
@@ -0,0 +1,19 @@
+package ru.bunkov.lesson3.stack;
+
+public interface Stack<E> {
+
+    void push(E value);
+
+    E pop();
+
+    E peek(); //top()
+
+    int size();
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    void display();
+
+}
